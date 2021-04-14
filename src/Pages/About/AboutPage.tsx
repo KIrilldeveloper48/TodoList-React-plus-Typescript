cbd453c721b06d13e1685db2b807d56e87c5e22f
Kirill <developerpopov@yandex.ru>
2021-04-14 13:18:20 +0300

    Изменил значки

--- src/Pages/About/AboutPage.tsx
+++ src/Pages/About/AboutPage.tsx
@@ -10,8 +10,8 @@ const AboutPage: React.FC = () => {
       <div className="badges">
         <img src="https://img.shields.io/badge/made%20by-KIrilldeveloper48-blue.svg" alt="badge" />
         <img src="https://img.shields.io/badge/react-16.14.0-green.svg" alt="badge" />
-        <img src="https://img.shields.io/badge/redux-7.2.2-green.svg" alt="badge" />
-        <img src="https://img.shields.io/github/languages/top/KIrilldeveloper48/Six-cities-SPA-React.svg" alt="badge" />
+        <img src="https://img.shields.io/badge/Typescript-4.2.4-green.svg" alt="badge" />
+        <img src="https://img.shields.io/github/languages/top/KIrilldeveloper48/TodoList-React-plus-Typescript.svg" alt="badge" />
       </div>
 
       <p>Простой Todo List для закрепления навыков React в связке с Typescript</p>
